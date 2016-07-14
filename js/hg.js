a8a0dbed7787993c760412e7a8a9b47bd2a98871
VyaDo <930881306@qq.com>
2016-07-15 02:06:56 +0800

    Update hg.js

--- js/hg.js
+++ js/hg.js
@@ -1,5 +1,19 @@
 $(function () {
 	
+	for (var i = 0; i < document.images.length; i ++) {
+		document.images[i].ondragstart = function () {
+			return false;
+		};
+	};
+	
+	
+	for (var i = 0; i < document.links.length; i ++) {
+		document.links[i].ondragstart = function () {
+			return false;
+		};
+	};
+	
+	
 	var incr = 60;
 	var screenW = window.screen.availWidth || document.documentElement.clientHeight;
 	var screenH = (window.screen.availHeight - window.outerHeight + window.innerHeight || document.documentElement.clientHeight) * 1.04;
@@ -17,10 +31,27 @@ $(function () {
 		width: screenW,
 		height: screenH,
 	});
+	
+	
 	$("#bg").css({
 		width: screenW + incr,
 		height: screenH + incr,
 	});
+	
+	
+	$("#musicApp").css({
+		width: screenW,
+		height: screenH,
+		top: 0,
+		left: screenW,
+	});
+	
+	
+	$("#screenCover").css({
+		width: screenW,
+		height: screenH,
+	});
+	
 	
 	var aLW = $("#asideLeft").width(),
 				aLH = $("#asideLeft").height(),
@@ -33,39 +64,140 @@ $(function () {
 		width: aLW,
 		height: aLH,
 	});
+	
+	
 	$("#divLeft .div_personalInfoItem .canvas_personalInfoItem").prop({
 		width: dLW * 0.5,
 		height: dLH * 0.09,
 	});
+	
+	
 	$("#divLeft .div_personalInfoItem .div_shadow").css({
 		height: dLH * 0.09,
 	});
+	
+	
 	/*$("#canvas_myApplication").prop({
 		width: $("#asideRight").width(),
 		height: $("#asideRight").height(), 
 	});*/
+	
+	
 	$("#div_myApplication").css({
 		width: aRW * 0.6 * 2,
 		height: aRW * 0.6 * 2,
 		right: -aRW * 0.6,
 		bottom: -aRW * 0.6 * 0.5,
 	});
+	
+	
+	// .app定位
+	$(".app").css({
+		left: aRW * 0.6 - 32,
+		top: aRW * 0.066,
+		transformOrigin: "32px " + aRW * 0.534 + "px",
+	}).each(function (index) {
+		$(this).css({
+			transform: "rotate(" + (-360 / $(".app").length * (index + 0.5)) + "deg)",
+		});
+	}).hover(function () {
+		$(this).css({
+			boxShadow: "0 0 5px #111",
+		});
+	}, function () {
+		$(this).css({
+			boxShadow: "none",			
+		});
+	}).on({
+		mousedown: function () {
+			$(this).css({
+				boxShadow: "0 0 2px #111",			
+			});
+		},
+		mouseup: function () {
+			$(this).css({
+				boxShadow: "0 0 5px #111",
+			});
+		},
+	});
+	
+	(function () {
+		var n = 0;
+		$("#div_myApplication").on("mousedown", function (ed) {
+			$(this).on("mousemove", function (em) {
+				if (em.pageY - ed.pageY > 30) {
+					++n;
+					$(this).off("mousemove");
+					$(this).css({
+						transform: "rotate(" + (-60 * n) + "deg)",
+					});
+				};
+				if (em.pageY - ed.pageY < -30) {
+					--n;
+					$(this).off("mousemove");
+					$(this).css({
+						transform: "rotate(" + (-60 * n) + "deg)",
+					});
+				};
+			});
+			$("#div_myApplication").on("mouseup", function () {
+				$(this).off("mousemove");
+			});
+		});
+	})();
+	
+	
+	$("#music").on("click", function () {
+		$("#musicApp").animate({
+			left: 0,
+		});
+	});
+	
+	
 	$("#div_moreApplication").css({
-		width: aRW * 0.3 * 2,
+		width: aRW * 0.55,
 		height: aRW * 0.3 * 2,
 		right: -aRW * 0.3,
 		bottom: 0,
-		lineHeight: aRW * 0.1,
-		marginLeft: aRW * 0.06,
-		fontSize: aRW * 0.06,
+		fontFamily: "微软雅黑",
+		fontWeight: "bold",
+		fontSize: aRW * 0.07,
+		paddingLeft: aRW * 0.05,
+		lineHeight: aRW * 0.022,
+		cursor: "pointer",
+	}).hover(function () {
+		$(this).css({
+			backgroundColor: "#f90",
+		});
+	}, function () {
+		$(this).css({
+			backgroundColor: "#fc0",
+		});
+	}).on({
+		mousedown: function () {
+			$(this).css({
+				boxShadow: "0 0 1px #333",
+			});
+		},
+		mouseup: function () {
+			$(this).css({
+				boxShadow: "0 0 3px #333",
+			});
+		},
 	});
+	
+	
 	$("#div_content").css({
 		height: 0,
 	});
+	
+	
 	$("#game").find("article").find("img").css({
 		width: $("#game").width() * 0.2,
 		height: $("#game").width() * 0.2,
 	})
+	
+	
 	$("#game").find("div").hover(function () {
 		$(this).find("img").css({
 			boxShadow: "0 0 1px #333",
@@ -82,7 +214,25 @@ $(function () {
 		});
 	});
 	
-		
+	
+	// 头像
+	$("#headPhoto img").on("mouseover", function () {
+		$(this).animate({
+			width: "175%",
+			height: "105%",
+			left: "-8%",
+			top: "-4%",
+		});
+	}).on("mouseout", function () {
+		$(this).animate({
+			width: "170%",
+			height: "100%",
+			left: "-3%",
+			top: 0,
+		});
+	});
+	
+	
 	(function init() {
 		// 背景动画 & 侧栏动画
 		var gen;
@@ -180,24 +330,6 @@ $(function () {
 	})();
 	
 	
-	// 头像
-	$("#headPhoto img").on("mouseover", function () {
-		$(this).animate({
-			width: "175%",
-			height: "105%",
-			left: "-8%",
-			top: "-4%",
-		});
-	}).on("mouseout", function () {
-		$(this).animate({
-			width: "170%",
-			height: "100%",
-			left: "-3%",
-			top: 0,
-		});
-	});
-	
-	
 	// 个人信息Item初始效果
 	ItemCanvasEffectUp("canvas_myIntroduction", "#ffa", "个人简介", "#fc0");
 	ItemCanvasEffectUp("canvas_myStudy", "#ffa", "我的学习", "#fc0");
@@ -206,21 +338,80 @@ $(function () {
 	
 	
 	$("#canvas_myIntroduction").click(function () {
+		$.ajax({
+			url: "json/myIntroduction.json",
+			dataType: "json",
+			success: function (data, stateText, jqXHR) {
+				var t = $.parseJSON(jqXHR.responseText);
+				$("#article_show").html(t[0]["introduction"]);
+			},
+		});
+		$("#screenCover").css({
+			display: "block",
+		});
 		$("#div_content").animate({
 			height: "80%",
 			marginTop: "10%",
 			marginBottom: "10%",
 		}, {
+			duration: 300,
 			done: function () {
 				$("#article_show").animate({
 					opacity: 1,
+				}, {
+					duration: 200,
 				});
 			},
 		});
 	});
 	
 	
+	$("#div_myStudy").on("click", function () {
+		$.ajax({
+			url: "json/myIntroduction.json",
+			dataType: "json",
+			success: function(data, statusText, jqXHR) {
+				var j = $.parseJSON(jqXHR.responseText);
+				if (j[1]["text"] == "") {
+					$("#list").html("<li>一篇还没有。。</li>");
+				} else {
+					var t = "";
+					for (var i = 0; i < j[1]["text"].length; i ++) {
+						t += "<li>" + j[1]["text"][i] + "</li>";
+					};
+					$("#list").html(t);
+				};
+			},
+		});
+		$("#screenCover").css({
+			display: "block",
+		});
+		$("#div_stu").fadeIn(500);
+	});
 	
+	
+	$("#screenCover").on("click", function () {
+		$("#div_stu").fadeOut(500);
+		$("#article_show").animate({
+			opacity: 0,
+		}, {
+			duration: 200,
+			done: function () {
+				$("#div_content").animate({
+					height: 0,
+					marginTop: "50%",
+					marginBottom: "50%",
+				}, {
+					duration: 300,
+					done: function () {
+						$("#screenCover").css({
+							display: "none",
+						});
+					},
+				});
+			},
+		});
+	});
 	
 	
 	// 左边Item的canvas normal效果
@@ -300,17 +491,115 @@ $(function () {
 				ctx.shadowColor = "rgba(0, 0, 0, 0)";
 				ctx.fillText(text, 50, 35);
 			});
-			
 		});
 	};
 	
 	
+	$("#articles").on("click", function () {
+		$.ajax({
+			url: "json/myIntroduction.json",
+			dataType: "json",
+			success: function(data, statusText, jqXHR) {
+				var j = $.parseJSON(jqXHR.responseText);
+				if (j[1]["text"] == "") {
+					$("#list").html("<li>一篇还没有。。</li>");
+				} else {
+					var t = "";
+					for (var i = 0; i < j[1]["text"].length; i ++) {
+						t += "<li>" + j[1]["text"][i] + "</li>";
+					};
+					$("#list").html(t);
+				};
+			},
+		});
+		$(this).off("mouseleave").css({
+			color: "#333",
+			boxShadow: "none",
+		});
+		$("#studyWebsite").css({
+			color: "#ccc",
+			boxShadow: "2px -1px 1px #ccc inset",
+		}).on("mouseleave", function () {
+			$(this).css({
+				color: "#ccc",
+			});
+		});
+	}).on("mouseenter", function () {
+		$(this).css({
+			color: "#333",
+		});
+	});
 	
-	// 关闭按钮效果
+	
+	$("#studyWebsite").hover(function () {
+		$(this).css({
+			color: "#333",
+		});
+	}, function () {
+		$(this).css({
+			color: "#ccc",
+		});
+	}).on("click", function () {
+		$.ajax({
+			url: "json/myIntroduction.json",
+			dataType: "json",
+			success: function(data, statusText, jqXHR) {
+				var j = $.parseJSON(jqXHR.responseText);
+				if (j[1]["website"] == "") {
+					$("#list").html("<li>一篇还没有。。</li>");
+				} else {
+					var t = "";
+					for (var i = 0; i < j[1]["website"].length; i ++) {
+						t += "<li><a href=" + j[1]["website"][i] + " target='_blank'>" + j[1]["website"][i] + "</a></li>";
+					};
+					$("#list").html(t);
+					$("#list").find("li").css({
+						marginBottom: "10px",
+					});
+					$("#list").find("a").hover(function () {
+						$(this).css({
+							color: "#333",
+						});
+					}, function () {
+						$(this).css({
+							color: "#999",
+						});
+					}).css({
+						color: "#999",
+						fontFamily: "微软雅黑",
+						textDecoration: "none",
+					});
+				};
+			},
+		});
+		$(this).off("mouseleave").css({
+			color: "#333",
+			boxShadow: "none",
+		});
+		$("#articles").on("mouseleave", function () {
+			$(this).css({
+				color: "#ccc",
+			});
+		}).css({
+			color: "#ccc",
+			boxShadow: "-2px -1px 1px #ccc inset",
+		});
+	});
+	
+	
+	$("#bgChange").add("#lvMessage").hover(function () {
+		$(this).find("img").prop("src", "img/icon_orange.png");
+	}, function () {
+		$(this).find("img").prop("src", "img/icon_white.png");
+	});
+	
+	
+	/*// 关闭按钮效果
 	(function () {
 		var div_content = $("#div_content"),
 					art = $("#article_show"),
-					closeButton = $(".closeButton");
+					closeButton = $(".closeButton"),
+					cover = $("#screenCover");
 		closeButton.hover(function () {
 			$(this).prop("src", "img/closeButton_hover.png");
 		}, function () {
@@ -319,20 +608,9 @@ $(function () {
 			$(this).prop("src", "img/closeButton_down.png");
 		}).on("mouseup", function () {
 			$(this).prop("src", "img/closeButton_hover.png");
-			art.animate({
-				opacity: 0,
-			}, {
-				duration: 300,
-				done: function () {
-					div_content.animate({
-						height: 0,
-						marginTop: "50%",
-						marginBottom: "50%",
-					});
-				},
-			});
+			
 		});
-	})();
+	})();*/
 	
 	
 	
@@ -354,14 +632,6 @@ $(function () {
 		ctx.stroke();
 		ctx.fill();
 	})();*/
-	$.getJSON("json/myIntroduction.json", function (data, textStatus, jqXHR) {
-		var str = $.parseJSON(jqXHR.responseText),
-					intr = str[0]["introduction"];
-		$("#article_show").html(intr);
-		
-	});
-	
-	
 	
 });
 
